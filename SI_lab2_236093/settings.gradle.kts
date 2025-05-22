1d4dda3a1ed286455560f5f67b7d6c3f4dc1e5b0
Damjan Zimbakov <zimbakovtech@gmail.com>
2025-05-22 21:55:53 +0200

    Java Gradle

---
+++ SI_lab2_236093/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "SI_lab2_236093"
---
+++ SI_lab2_236093/src/test/java/SILab2Test.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SILab2Test {
+
+    @Test
+    void checkCart() {
+        List<Item> items = new ArrayList<Item>();
+    }
+}
